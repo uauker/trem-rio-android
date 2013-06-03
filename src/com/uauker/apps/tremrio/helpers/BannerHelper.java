7544bb3b639821a1d35b2a50d3717f4a40474ff6
Paulo Guilherme <uauker@gmail.com>
2013-06-02 21:16:31 -0300

    Fechando a versão 1.0

--- src/com/uauker/apps/tremrio/helpers/BannerHelper.java
+++ src/com/uauker/apps/tremrio/helpers/BannerHelper.java
@@ -12,7 +12,7 @@ public class BannerHelper {
 		AdView adView = (AdView)v.findViewById(R.id.adView);
 		
 		AdRequest request = new AdRequest();
-		request.addTestDevice("47764E7BF45BF0D60DD4D7185D18B260");
+//		request.addTestDevice("47764E7BF45BF0D60DD4D7185D18B260");
 		
 		adView.loadAd(request);
 		
