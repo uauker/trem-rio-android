46823b548e800a0f3a3cbd6d8d9711fe16637880
Paulo Guilherme <uauker@gmail.com>
2014-01-05 23:28:11 -0200

    Configuracao da lib com maior eficiencia

--- TremRio/src/com/uauker/apps/tremrio/TremRioApplication.java
+++ TremRio/src/com/uauker/apps/tremrio/TremRioApplication.java
@@ -4,20 +4,19 @@ import java.io.File;
 
 import android.app.Application;
 import android.content.Context;
-import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Bitmap.Config;
 
 import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
-import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 import com.nostra13.universalimageloader.utils.StorageUtils;
 
 public class TremRioApplication extends Application {
-	
+
 	private static Context globalApplicationContext;
 
 	@Override
@@ -36,19 +35,17 @@ public class TremRioApplication extends Application {
 		DisplayImageOptions options = new DisplayImageOptions.Builder()
 				.showStubImage(R.drawable.ic_launcher)
 				.showImageForEmptyUri(R.drawable.ic_launcher)
-				.showImageOnFail(R.drawable.ic_launcher)
-				.resetViewBeforeLoading(true).cacheOnDisc(true)
+				.showImageOnFail(R.drawable.ic_launcher).cacheInMemory(false)
 				.imageScaleType(ImageScaleType.EXACTLY_STRETCHED)
+				.bitmapConfig(Config.RGB_565).cacheOnDisc(true)
 				.displayer(new FadeInBitmapDisplayer(300)).build();
 
 		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
 				globalApplicationContext).defaultDisplayImageOptions(options)
-				.discCacheExtraOptions(480, 800, CompressFormat.JPEG, 75, null)
 				.denyCacheImageMultipleSizesInMemory()
-				.threadPriority(Thread.NORM_PRIORITY - 2)
-				.memoryCache(new LruMemoryCache(2 * 1024 * 1024))
-				.memoryCacheSize(2 * 1024 * 1024).threadPoolSize(1)
-				.tasksProcessingOrder(QueueProcessingType.FIFO)
+				.memoryCache(new WeakMemoryCache()).threadPoolSize(5)
+				.threadPriority(Thread.MIN_PRIORITY + 3)
+				.memoryCacheSize(2 * 1024 * 1024)
 				.discCache(new UnlimitedDiscCache(cacheDir))
 				.discCacheSize(50 * 1024 * 1024).discCacheFileCount(100)
 				.build();
